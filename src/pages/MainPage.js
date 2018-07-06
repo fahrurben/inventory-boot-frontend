8ed34edc652621a94165de68784177c400af5b5a
fahrur <fahrurben@gmail.com>
2018-07-06 10:04:30 +0700

    Navbar change color

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -24,23 +24,36 @@ class MainPage extends Component {
       <div className="container">
         {
           isLoggedIn() &&
-          <nav className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-              <a className="navbar-item" href="https://bulma.io">
-                <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
-              </a>
+          <div className="columns">
+            <div className="column">
+              <nav className="navbar is-primary" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                  <a className="navbar-item" href="https://bulma.io">
+                    <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28" />
+                  </a>
 
-              <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-              </a>
+                  <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                  </a>
+                </div>
+                <div className="navbar-menu">
+                  <div class="navbar-item has-dropdown is-hoverable">
+                    <a class="navbar-link">
+                      Data
+                    </a>
+                    <div class="navbar-dropdown is-boxed">
+                      <Link to="/customerList" className="navbar-item">Customer</Link>
+                    </div>
+                  </div>
+                  <div className="navbar-end">
+                    <a className="navbar-item" onClick={this.doLogout}>Logout</a>
+                  </div>  
+                </div>
+              </nav>
             </div>
-            <div className="navbar-menu">
-              <Link to="/customerList" className="navbar-item">Customer</Link>
-              <a className="navbar-item" onClick={this.doLogout}>Logout</a>
-            </div>
-          </nav>
+          </div>
         }
         <Switch>
           <Route path="/home" component={HomePage} />  
